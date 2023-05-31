c2550d30b3855cbd3f767a3d6e3c11a5fb7ac194
Semeniuk Mykhailo <micanton0709@gmail.com>
2023-05-30 23:21:21 -0400

    .

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const list = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map((image) => `<li>
+    <a  class="gallery__link" href="${image.original}">
+        <img class="gallery__image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}">
+    </a>
+  </li>`)
+  .join("");
+list.insertAdjacentHTML("beforeend", markup);
